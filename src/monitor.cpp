ae38c6a53007efb69338a9a82e84a89a4a0e5f42
Taucrates <tonitauler94@gmail.com>
2022-01-25 16:38:02 +0100

    in development

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -8,6 +8,10 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <fuzzymar_multi_robot/mission.h>
+#include <fuzzymar_multi_robot/missionArray.h>
+#include <fuzzymar_multi_robot/action_task.h>
+#include "visualization_msgs/MarkerArray.h"
+#include "visualization_msgs/Marker.h"
 #include <stdio.h>
 #include <fstream>
 #include <string>
@@ -15,16 +19,55 @@
 #include <vector>
 #include <iomanip>
 
+// STRUCTS
+
+struct Mission {
+    uint8_t id_task;
+    float deadline;
+		float weight;
+		float x;
+		float y;
+		uint8_t doing_task;
+};
+
+// FLAGS
 bool first_loop = true;
-std::string state_pub_topic = "mission_state";
+bool missions_update = false;
+
+// TOPICS
+std::string state_pub_topic = "/mission_state";
+
+// Global Variable where save the mission
+//std::vector<std::vector<float>> missions; // vector where the tasks are saved {id_task, deadline, weight, x, y, z, doing_task}
+std::vector<Mission> missions;
+
+// Functions Declaration
+void getMission(std::vector<Mission>* missions, const std::string& mission_path_file);
+void publishMission(ros::Publisher* mission_pub);
+void publishMarkers(ros::Publisher* vis_pub);
+
+/********************************************************************************************
+*************************************** CALLBACKS *******************************************
+********************************************************************************************/
+
+void taskUpdateCallback(const fuzzymar_multi_robot::action_task::ConstPtr& task)
+{
+	for(int i = 0 ; i < missions.size() ; i++)
+	{
+		if((int)missions[i].id_task == (int)task->id_task)   // updating the doing_task variable
+		{
+			missions[i].doing_task += 1;   
+		}
+	}
+
+	missions_update = true;
+}
 
 
-// Functions
-void getMission(std::vector<std::vector<float>>* missions, const std::string& mission_path_file);
-void publishMission(const std::vector<float>& mission_vect, ros::Publisher* mission_pub);
+/********************************************************************************************
+***************************************** MAIN **********************************************
+********************************************************************************************/
 
-
-// Main
 int main(int argc, char **argv)
 {
 
@@ -32,17 +75,19 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Publisher mission_pub = n.advertise<fuzzymar_multi_robot::mission>(state_pub_topic, 10);
+	// Subscribers
+  ros::Subscriber task_sub = n.subscribe("/treball", 1000, taskUpdateCallback);
 
-  // Global Variable where save the mission
-  std::vector<std::vector<float>> missions;
+	// Publishers
+  ros::Publisher mission_pub = n.advertise<fuzzymar_multi_robot::missionArray>(state_pub_topic, 10);
+  ros::Publisher vis_pub = n.advertise<visualization_msgs::MarkerArray>( "visualization_marker_array", 0 );
 
-  // Global Variable as buffer to read de keyboard 
+  // Variable as buffer to read de keyboard 
   char cmd[1];
 
   // Getting path to .txt file (mission)
   std::string mission_path_file;
-  n.getParam("/monitor/mission_path_file", mission_path_file);   // "/node_name/param_name"
+  n.getParam("/monitor/mission_path_file", mission_path_file);   // "/node_name(indicated inside this code)/param_name"
 
   ros::Rate loop_rate(10);
 
@@ -58,15 +103,33 @@ int main(int argc, char **argv)
 
         getMission(& missions, mission_path_file);    // get the info of the current mission
 
-        // VERY FIRST publish mission
-        for(int i = 0; i < missions.size(); i++){
-            publishMission(missions[i], &mission_pub);
+        for(int i = 0 ; i < missions.size() ; i++)
+        {
+          ROS_INFO("ID: %i , DD: %f , WEIGTH: %f , X: %f , Y: %f , Robots: %i ", missions[i].id_task, missions[i].deadline, missions[i].weight, missions[i].x, missions[i].y, missions[i].doing_task);
         }
+
+        // VERY FIRST publish mission
+        publishMission(&mission_pub);
+        
 
         ROS_INFO("Missions have been published for the first time");
 
         first_loop = false;
     }
+
+		if(missions_update) // publish the vector missions when is updated
+		{
+			publishMission(&mission_pub);
+
+			for(int i = 0 ; i < missions.size() ; i++)
+			{
+				ROS_INFO("ID: %i , DD: %f , WEIGTH: %f , X: %f , Y: %f , Robots: %i ", missions[i].id_task, missions[i].deadline, missions[i].weight, missions[i].x, missions[i].y, missions[i].doing_task);
+			}
+
+			missions_update = false;
+		}
+
+    publishMarkers(&vis_pub);
 
     ros::spinOnce();
 
@@ -76,7 +139,13 @@ int main(int argc, char **argv)
   return 0;
 }
 
-void getMission(std::vector<std::vector<float>>* missions, const std::string& mission_path_file){
+
+/********************************************************************************************
+*************************************** FUNCTIONS *******************************************
+********************************************************************************************/
+
+void getMission(std::vector<Mission>* missions, const std::string& mission_path_file)
+{
 
     ROS_DEBUG("Inside getMission");
     // clearing mission vector
@@ -88,32 +157,77 @@ void getMission(std::vector<std::vector<float>>* missions, const std::string& mi
     while (std::getline(inputFile, line))
     {
         std::istringstream iss(line);
-        float id, deadline, weight, x, y, z;
-        if (!(iss >> id >> deadline >> weight >> x >> y >> z)) { break; } // error
+				Mission aux_mission;
+				float task_id, z;
+				if (!(iss >> task_id >> aux_mission.deadline >> aux_mission.weight >> aux_mission.x >> aux_mission.y >> z)) { break; } // error
+				aux_mission.id_task = (uint8_t)task_id;
+				aux_mission.doing_task = 0;
 
-        // building mission vector
-        std::vector<float> aux_mission = {id, deadline, weight, x, y, z};
-        missions->push_back(aux_mission);
-        ROS_DEBUG("ID: %f , DD: %f , WEIGTH: %f , X: %f , Y: %f ", aux_mission[0], aux_mission[1], aux_mission[2], aux_mission[3], aux_mission[4]);
+				missions->push_back(aux_mission);
     }
     
 }
 
 
-void publishMission(const std::vector<float>& mission_vect, ros::Publisher* mission_pub){
+void publishMission(ros::Publisher* mission_pub)
+{
 
     ROS_DEBUG("Inside publishMission");
     // buffer to be published of 'mission.msg' type
-    fuzzymar_multi_robot::mission mission_buff;
+    fuzzymar_multi_robot::mission mission_line;
+		fuzzymar_multi_robot::missionArray mission_buff;
     
-    mission_buff.id_task = mission_vect[0];
-    mission_buff.deadline_task = mission_vect[1];
-    mission_buff.weight_task = mission_vect[2];
-    mission_buff.location_task.x = mission_vect[3];
-    mission_buff.location_task.y = mission_vect[4];
-    mission_buff.location_task.z = mission_vect[5];
+		for(int i = 0 ; i < missions.size() ; i++)
+		{
+			mission_line.id_task = missions[i].id_task;
+			mission_line.deadline_task = missions[i].deadline;
+			mission_line.weight_task = missions[i].weight;
+			mission_line.x = missions[i].x;
+			mission_line.y = missions[i].y;
+			mission_line.doing_task = missions[i].doing_task;
 
-    ROS_DEBUG("ID: %f , DD: %f , WEIGTH: %f , X: %f , Y: %f ", mission_vect[0], mission_vect[1], mission_vect[2], mission_vect[3], mission_vect[4]);
+			mission_buff.missions.push_back(mission_line);
+		}
+    
+		mission_buff.vector_size = missions.size();
+    //ROS_DEBUG("ID: %f , DD: %f , WEIGTH: %f , X: %f , Y: %f ", mission_vect[0], mission_vect[1], mission_vect[2], mission_vect[3], mission_vect[4]);
    
     mission_pub->publish(mission_buff);
+}
+
+void publishMarkers(ros::Publisher* vis_pub)
+{
+    visualization_msgs::MarkerArray marker;
+    visualization_msgs::Marker aux_marker;
+
+    for(int i = 0 ; i < missions.size() ; i++)
+    {
+        aux_marker.header.frame_id = "map";
+        aux_marker.header.stamp = ros::Time::now();
+        aux_marker.ns = "tasks";
+        aux_marker.id = i;
+        aux_marker.type = visualization_msgs::Marker::CYLINDER;
+        aux_marker.action = visualization_msgs::Marker::ADD;
+        aux_marker.pose.position.x = missions[i].x;
+        aux_marker.pose.position.y = missions[i].y;
+        aux_marker.pose.position.z = 0.5;
+        aux_marker.pose.orientation.x = 0.0;
+        aux_marker.pose.orientation.y = 0.0;
+        aux_marker.pose.orientation.z = 0.0;
+        aux_marker.pose.orientation.w = 1.0;
+        aux_marker.scale.x = 0.1;
+        aux_marker.scale.y = 0.1;
+        aux_marker.scale.z = 1.0;
+        aux_marker.color.a = 1.0; // Don't forget to set the alpha!
+        aux_marker.color.r = 1.0;
+        aux_marker.color.g = 0.0;
+        aux_marker.color.b = 0.0;
+
+        marker.markers.push_back(aux_marker);
+
+        //vis_pub->publish(marker.markers[i]);
+    }
+
+    vis_pub->publish(marker);
+
 }
--- src/robot_operation.cpp
+++ src/robot_operation.cpp
@@ -7,7 +7,13 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "actionlib_msgs/GoalStatus.h"
+#include "actionlib_msgs/GoalStatusArray.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include <fuzzymar_multi_robot/mission.h>
+#include <fuzzymar_multi_robot/missionArray.h>
+#include <fuzzymar_multi_robot/action_task.h>
 #include <stdio.h>
 #include <fstream>
 #include <string>
@@ -15,52 +21,189 @@
 #include <vector>
 #include <iomanip>
 
+#define DEBUGGING 1
+
+// STRUCTS
+
+struct Mission {
+    uint8_t id_task;
+    float deadline;
+		float weight;
+		float x;
+		float y;
+		uint8_t doing_task;
+    bool enable;
+};
+
+// GLOBAL VARIABLES
+int kobuki_id;
+std::string state_sub_topic = "/mission_state";
+std::string pose_sub_topic = "amcl_pose";
+std::string goal_status_sub_topic = "move_base/status";
+
+std::vector<Mission> missions; // vector where the tasks are saved {id_task, deadline, weight, x, y, doing_task, enable}
+std::vector<std::vector<float>> probabilities; // vector where probabilities are saved
+float threshold = 0.0; // threshold
+std::pair<float, float> current_position; // current position of the robot
+std::vector<float> current_goal; // current task declared as objective {task_id, x, y, yaw}
+
+uint8_t status = 255; // objective status of the robot -> 1 == in the way to goal // 3 == goal reached // 255 == no status 
+
+ros::Time init_task; // time at which current task begins
+
+// FLAGS
+bool first_loop = true;
 bool new_mission_state = false;
-std::string state_sub_topic = "mission_state";
+bool way_to_task = false; // indicates if the robot is on the way of a task
+bool send_action_task = false; // allow to send action_task msg
 
-// Global Variable where save the mission
-std::vector<std::vector<float>> missions;
+// FUNCTIONS DECLARATION
+float distance(float x1, float y1, float x2, float y2); // calculate the distance between 2 points in 2D
+float getThreshold(); // get the threshold == max_dist(between tasks)/2
+float getStimulous(int task); // get the current robot stimulous for a given task
+void setProbabilities(); // build the probabilities vector {id_task, stimulous, probability}
+std::vector<float> setTaskObjective(); // return the global goal {task_id, x, y, yaw} to publish to /kobuki_x/move_base_simple/goal
 
-void missionStateCallback(const fuzzymar_multi_robot::mission::ConstPtr& mission)
+void publishGoal(ros::Publisher* goal_pub, std::vector<float> goal);
+void publishTask(ros::Publisher* task_pub);
+
+/********************************************************************************************
+*************************************** CALLBACKS *******************************************
+********************************************************************************************/
+
+void missionStateCallback(const fuzzymar_multi_robot::missionArray::ConstPtr& mission)
 {
-  
-  float id, deadline, weight, x, y; // In a 3D navigation have to add z pose
 
-  id = (float)mission->id_task;
+  /*float id_task, deadline, weight, x, y, doing_task, enable; // In a 3D navigation have to add z pose
+  id_task = (float)mission->id_task;
   deadline = mission->deadline_task;
   weight = mission->weight_task;
   x = mission->location_task.x;
   y = mission->location_task.y;
+  doing_task = 0.0; // 0 robots doing the task
+  enable = 1.0; // is enable
+
   //z = mission->location_task.z;  // 3D navigation
 
   // building mission vector
-  std::vector<float> aux_mission = {id, deadline, weight, x, y}; // In a 3D navigation have to add z pose
+  std::vector<float> aux_mission = {id_task, deadline, weight, x, y, doing_task, enable}; // In a 3D navigation have to add z pose
 
-  missions.push_back(aux_mission);
+  missions.push_back(aux_mission);*/
+
+  Mission mission_data;
+  
+  for(int i = 0 ; i < mission->vector_size ; i++)
+  {
+    mission_data.id_task = mission->missions[i].id_task;
+    mission_data.deadline = mission->missions[i].deadline_task;
+    mission_data.weight = mission->missions[i].weight_task;
+    mission_data.x = mission->missions[i].x;
+    mission_data.y = mission->missions[i].y;
+    mission_data.doing_task = mission->missions[i].doing_task;
+    if(mission->missions[i].doing_task <= 4){mission_data.enable = true;}
+
+    missions.push_back(mission_data);
+  }
 
   new_mission_state = true;
   
 }
 
+void actualPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose)
+{
+  
+  current_position.first = pose->pose.pose.position.x;
+  current_position.second = pose->pose.pose.position.y;
+  
+}
+
+void statusGoalCallback(const actionlib_msgs::GoalStatusArray::ConstPtr& stat)
+{
+  
+  if (!stat->status_list.empty())  // we have to ensure status_list array is not empty else we have error and node die due to memory allocation error
+  {
+    status = stat -> status_list[stat->status_list.size()-1].status; // this structure is used to always have the status updated (due to move_base way to work)
+  } else {
+    status = 255;
+  }
+
+  if(status == 3 && way_to_task)
+  {
+    init_task = stat -> header.stamp;
+    send_action_task = true;
+    way_to_task = false;
+    ROS_DEBUG("Task: %i initialized by Kobuki_%i at sec: %i  nsec: %i", (int)current_goal[0], kobuki_id, init_task.sec, init_task.nsec);
+  } else if(status == 1){
+    way_to_task = true;
+  }
+
+}
+/********************************************************************************************
+***************************************** MAIN **********************************************
+********************************************************************************************/
+
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "robot_operator");
+  ros::init(argc, argv, "robot_operation");
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe(state_sub_topic, 1000, missionStateCallback);
+  // Subscribers
+  ros::Subscriber mission_sub = n.subscribe(state_sub_topic, 1000, missionStateCallback);
+  ros::Subscriber pose_sub = n.subscribe(pose_sub_topic, 1000, actualPoseCallback);
+  ros::Subscriber goal_status_sub = n.subscribe(goal_status_sub_topic, 1000, statusGoalCallback);
+
+  // Publishers
+  ros::Publisher goal_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 10);
+  ros::Publisher task_pub = n.advertise<fuzzymar_multi_robot::action_task>("/treball", 10);
+
+  // Params
+  n.getParam("robot_operation/kobuki_id", kobuki_id);   // "/node_name(indicated inside this code)/param_name" --> the first '/' depends if groupns is used or not
 
   ros::Rate loop_rate(10);
 
   while (ros::ok())
   {
     // When new mission state is received by monitor
-    if(new_mission_state)
+    if(new_mission_state && first_loop)
     {
-      for(int i = 0 ; i < missions.size() ; i++)
+
+      threshold = getThreshold(); // the threshold is gotten
+
+      setProbabilities(); // the probabilities for each task are obtained
+
+      current_goal = setTaskObjective(); // set de current objective (task) for the robot
+
+      publishGoal(&goal_pub, current_goal); // the current_goal is published on /kobuki_x/move_base_simple/goal topic
+
+      // ***************DUBUGGING*****************
+
+      if(DEBUGGING)
       {
-        ROS_INFO("ID: %f , DD: %f , WEIGTH: %f , X: %f , Y: %f", missions[i][0], missions[i][1], missions[i][2], missions[i][3], missions[i][4]);
+        // KOBUKI
+        ROS_INFO("kobuki_%i initialized", kobuki_id);
+
+        // Missions vector
+        for(int i = 0 ; i < missions.size() ; i++)
+        {
+          ROS_INFO("ID: %i , DD: %f , WEIGTH: %f , X: %f , Y: %f , Robots: %i , ENABLE(robots < ports):%i ", missions[i].id_task, missions[i].deadline, missions[i].weight, missions[i].x, missions[i].y, missions[i].doing_task, (int)missions[i].enable);
+        }
+
+        // Threshold
+        ROS_INFO("Threshold: %f", threshold);
+
+        // Probabilities vector
+        for(int i = 0 ; i < probabilities.size() ; i++)
+        {
+          ROS_INFO("ID: %i , STIMULOUS: %f , PROBABILITY: %f", (int)probabilities[i][0], probabilities[i][1], probabilities[i][2]);
+        }
+
+        // Current_goal vector
+        ROS_INFO("Current task -> ID: %i , X: %f , Y: %f , YAW: %f", (int)current_goal[0], current_goal[1],current_goal[2], current_goal[3]);
+
+        // Current position
+        ROS_INFO("Current position -> X: %f, Y: %f", current_position.first, current_position.second);
       }
 
       //TODO
@@ -69,14 +212,136 @@ int main(int argc, char **argv)
       //TODO
 
       new_mission_state = false;
+      first_loop = false;
 
+    }
+
+    if(send_action_task) // send when robot arrives to the current task (goal reached)
+    {
+      // publish
+      publishTask(&task_pub); // publish {id_kobuki, id_task, sec, nsec}
+      send_action_task = false;
     }
 
     ros::spinOnce();
 
     loop_rate.sleep();
-
-  }
+    
+  } 
 
   return 0;
+}
+
+/********************************************************************************************
+*************************************** FUNCTIONS *******************************************
+********************************************************************************************/
+
+float distance(float x1, float y1, float x2, float y2)
+{
+  float dist = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+  return dist;
+}
+
+float getThreshold()
+{
+  float aux_thres = 0.0;
+  float dist = 0.0;
+  for(int i = 0 ; i < missions.size() - 1 ; i++)
+  {
+    for (int j = 1 ; j < missions.size() ; j++)
+    {
+
+      dist = distance(missions[i].x, missions[i].y, missions[j].x, missions[j].y);
+
+      if(aux_thres < dist)
+      {
+        aux_thres = dist;
+      }
+    }
+  }
+
+  float threshold = aux_thres/2;
+  //ROS_INFO("Threshold: %f", threshold);
+  return threshold;
+}
+
+float getStimulous(int task)
+{
+  float stim = 1 / distance(current_position.first, current_position.second, missions[task-1].x, missions[task-1].y); // TODO change 0.0's to /kobuki_x/amcl_pose x and y
+
+  return stim;
+}
+
+void setProbabilities()
+{
+  probabilities.clear();
+
+  float id, stimulous, probability;
+
+  for(int i = 1 ; i <= missions.size() ; i++)
+  {
+    id = (float)i;
+    stimulous = getStimulous(i); // i+1 because we give the id task not the vector position
+    probability = (stimulous*stimulous) / ((stimulous*stimulous) + (threshold*threshold));
+
+    std::vector<float> aux_prob = {id , stimulous, probability};
+
+    probabilities.push_back(aux_prob);
+  }
+
+}
+
+std::vector<float> setTaskObjective()
+{
+  std::vector<float> aux_objective;
+  float id, x, y, yaw;
+  float prob = 0.0;
+
+  for(int i = 0 ; i < probabilities.size() ; i ++)
+  {
+    if(probabilities[i][2] > prob)
+    {
+      prob = probabilities[i][2];
+      id = missions[i].id_task;
+      x = missions[i].x;
+      y = missions[i].y;
+      yaw = 0.0;  // TODO es ports per a cada missio
+    }
+  }
+
+  aux_objective = {id, x, y, yaw};
+  return aux_objective;
+
+}
+
+void publishGoal(ros::Publisher* goal_pub, std::vector<float> goal)
+{
+  geometry_msgs::PoseStamped goal_msg;
+
+  goal_msg.header.frame_id = "map";
+  goal_msg.header.stamp = ros::Time::now();
+  goal_msg.pose.position.x = goal[1];
+  goal_msg.pose.position.y = goal[2];
+  goal_msg.pose.position.z = 0.0;
+
+  goal_msg.pose.orientation.x = 0.0;
+  goal_msg.pose.orientation.y = 0.0;    // TODO fer l'orientació com toca 
+  goal_msg.pose.orientation.z = 0.0;
+  goal_msg.pose.orientation.w = 1.0;  
+
+  goal_pub->publish(goal_msg);
+}
+
+void publishTask(ros::Publisher* task_pub)
+{
+  fuzzymar_multi_robot::action_task aux_task;
+
+  aux_task.id_kobuki = kobuki_id;
+  aux_task.id_task = (uint8_t)current_goal[0];
+  aux_task.sec = init_task.sec;
+  aux_task.nsec = init_task.nsec;
+
+  ROS_INFO("kobuki_%i starts task %i at sec:%i nsec: %i", aux_task.id_kobuki, aux_task.id_task, aux_task.sec, aux_task.nsec);
+
+  task_pub->publish(aux_task);
 }
